d2419dc8c99cad409cac02c56a9401f209179132
ManojPandi001 <manojkumarpandit.mk@gmail.com>
2022-11-02 20:12:51 +0530

    prime factors correct code

--- Prime.js
+++ Prime.js
@@ -1,14 +1,23 @@
-function checkPrime(num){
-    for(let i=i; i<=num; i++){
-        if(num%i==1){
-            return true;
-        }else{
-            return false;
+function checkPrime(number){
+    let factor=0;
+    for(let i=1; i<=number; i++){
+        if(number%i==0){
+            factor=0;
         }
+    }
+    if(factor==2){
+        return true;
+    }else {
+        return false;
     }
     
     
 }
 
 let ans=checkPrime(17)
+if(ans==true){
+    console.log("Prime Number");
+}else{
+    console.log("Not a Prime Number");
+}
 
