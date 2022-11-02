9e653eb314bdb682f82b2f073b385da66299b35b
ManojPandi001 <manojkumarpandit.mk@gmail.com>
2022-11-02 13:25:52 +0530

    added prime code

---
+++ Prime.js
@@ -0,0 +1,14 @@
+function checkPrime(num){
+    for(let i=i; i<=num; i++){
+        if(num%i==1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    
+}
+
+let ans=checkPrime(17)
+
